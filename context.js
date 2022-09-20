a22478117dcd4fa21b8220ae4e4edcc66e556d72
Mister.Idmir <ccsstm@gmail.com>
2022-09-20 18:22:55 -0300

    segunda atualização
    atualização do meu site versão 2.0

---
+++ context.js
@@ -0,0 +1,7 @@
+function foco(){scroll(0, 1000)}
+window.onload = foco()
+
+function abrirAgro(){document.getElementById("agri").style.display = "flex"}
+function abrirInf(){document.getElementById("inf").style.display = "flex"}
+function fecharAgro(){document.getElementById("agri").style.display = "none"}
+function fecharInf(){document.getElementById("inf").style.display = "none"}
