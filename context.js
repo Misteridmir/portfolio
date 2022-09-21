fa42a7778abc6f8f27b857d534b2e228a464bf3e
Mister.Idmir <ccsstm@gmail.com>
2022-09-21 17:17:44 -0300

    versão 2.2.1
    a versão anterior (2.2) havia erros de logica como a fuga de seletores e etc. ... e erros de digitação q indeferiam o site. A nova versão não possui incremento de tecnologia ou informação do site é a apenas uma versão anterior consertada. Agora vai Pai!

--- context.js
+++ context.js
@@ -2,17 +2,16 @@ function foco(){scroll(0, 1000)}
 window.onload = foco()
 
 function abrirAgro(){document.getElementById("agri").style.display = "flex"
-///document.getElementById("Agro").style.height ="95%"  
-//document.getElementById("Agro").style.width ="48%"                
+        
 }
 function abrirInf(){document.getElementById("inf").style.display = "flex"
 }
 function fecharAgro(){document.getElementById("agri").style.display = "none"
-//document.getElementById("Agro").style.height ="89%"
-//document.getElementById("Agro").style.width ="42%" 
+
 }
 function fecharInf(){document.getElementById("inf").style.display = "none"
 }
 
-//width: 42%;
-//height: 89%;
+function teste1() {
+    window.open('Agro.html', '_blank');
+}
