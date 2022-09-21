15d217279297eb9ad0cb2d51cef1de2333486f6c
Mister.Idmir <ccsstm@gmail.com>
2022-09-21 16:51:11 -0300

    versão 2.2
    a versão anterior fez a função de idealizar o site , esta nova é mais responsiva, organizada e interativa (ainda n é responsiva ao ponto de ser aberta em celulares, pelo menos nem tanto!)

--- context.js
+++ context.js
@@ -1,7 +1,18 @@
 function foco(){scroll(0, 1000)}
 window.onload = foco()
 
-function abrirAgro(){document.getElementById("agri").style.display = "flex"}
-function abrirInf(){document.getElementById("inf").style.display = "flex"}
-function fecharAgro(){document.getElementById("agri").style.display = "none"}
-function fecharInf(){document.getElementById("inf").style.display = "none"}
+function abrirAgro(){document.getElementById("agri").style.display = "flex"
+///document.getElementById("Agro").style.height ="95%"  
+//document.getElementById("Agro").style.width ="48%"                
+}
+function abrirInf(){document.getElementById("inf").style.display = "flex"
+}
+function fecharAgro(){document.getElementById("agri").style.display = "none"
+//document.getElementById("Agro").style.height ="89%"
+//document.getElementById("Agro").style.width ="42%" 
+}
+function fecharInf(){document.getElementById("inf").style.display = "none"
+}
+
+//width: 42%;
+//height: 89%;
