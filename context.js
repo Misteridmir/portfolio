cc46b14206f68b0189b744b826bd93b4edf28932
Mister.Idmir <ccsstm@gmail.com>
2022-09-27 23:19:55 -0300

    versão 3.2
    Cabeçalho com animação foi colocado no index. Além de diminuir tamanhos de alocação além do sistema de menus das paginas resolvidos. Ainda sem JS!

--- context.js
+++ context.js
@@ -1,10 +1,13 @@
 window.alert("No atual momento o site não está adaptado á ser visualizado em celulares!")
 //function foco(){scroll(0, 1000)}
-window.onload = foco()
+
 
 function carta(lugar, flex){document.getElementById(lugar).style.display = flex}
 
 function enviar(link) {
     window.open(link, '_self');
 }
-function focar(foco){document.getElementById(foco).focus({preventScroll:true});}
+
+
+  
+ 
